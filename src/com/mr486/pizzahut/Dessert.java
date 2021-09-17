706176a98ebf70ba3e67c0bc7eba789b2d69ffa4
monsieur486 <monsieur486@gmail.com>
2021-09-17 17:33:20 +0200

    Ajout JavaDoc

--- src/com/mr486/pizzahut/Dessert.java
+++ src/com/mr486/pizzahut/Dessert.java
@@ -5,64 +5,60 @@ import java.text.DecimalFormat;
 /**
  * The type Dessert.
  */
-public abstract class Dessert
-{
-	private String libelle;// Libellé du dessert.
-	private double prix;// Prix du dessert.
+public abstract class Dessert {
+  private static final DecimalFormat format = new DecimalFormat("####0.00");
+  private String libelle; // Libellé du dessert.
+  private double prix; // Prix du dessert.
 
   /**
-   * Gets libelle.
+   * Accesseurs en lecture pour le libellé.
    *
-   * @return the libelle
+   * @return le libellé
    */
-// Accesseurs en lecture pour le libellé et le prix.
-	public String getLibelle()
-	{
-		return libelle;
-	}
+  public String getLibelle() {
+    return libelle;
+  }
 
-  /**
-   * Gets prix.
-   *
-   * @return the prix
-   */
-  public double getPrix()
-	{
-		return prix;
-	}
 
   /**
    * Sets libelle.
    *
-   * @param libelle the libelle
+   * @param libelle le libellé
    */
-// Accesseurs en écriture pour le libellé et le prix.
-	protected void setLibelle(String libelle)
-	{
-		this.libelle = libelle;
-	}
+  protected void setLibelle(String libelle) {
+    this.libelle = libelle;
+  }
 
   /**
-   * Sets prix.
+   * Accesseurs en lecture pour le prix.
    *
-   * @param prix the prix
+   * @return le prix
    */
-  protected void setPrix(double prix)
-	{
-		this.prix = prix;
-	}
+  public double getPrix() {
+    return prix;
+  }
 
-	private static final DecimalFormat format = new DecimalFormat("####0.00");
+  /**
+   * Accesseurs en écriture pour le prix.
+   *
+   * @param prix le prix
+   */
+  protected void setPrix(double prix) {
+    this.prix = prix;
+  }
 
-	// Méthode utilisée pour l'affichage d'un dessert.
-	public String toString()
-	{
-		var sb = new StringBuilder(getLibelle());
-		sb.append(" Total: ");
-		sb.append(format.format(getPrix()));
-		sb.append("€ dont ");
-		sb.append(format.format((getPrix()-getPrix()/(1 + config.taux_tva))));
-		sb.append("€ de TVA");
-		return sb.toString();
-	}
+  /**
+   * Affiche facture.
+   *
+   * @return affichage complet de la facture
+   */
+  public String toString() {
+    var sb = new StringBuilder(getLibelle());
+    sb.append(" Total: ");
+    sb.append(format.format(getPrix()));
+    sb.append("€ dont ");
+    sb.append(format.format((getPrix() - getPrix() / (1 + config.taux_tva))));
+    sb.append("€ de TVA");
+    return sb.toString();
+  }
 }
